a01bec75c9638def44016726444092dfc310e210
Bohdan Bashynskyi <bbashins@pl.ibm.com>
2017-04-27 14:05:35 +0200

    Small fix ssh

--- ssh/manifests/init.pp
+++ ssh/manifests/init.pp
@@ -9,20 +9,20 @@ class ssh::config {
 		'/etc/ssh/sshd_config':
                 ensure => present,
                 owner => 'root',
-                group => root,
+                group => 'root',
                 mode => 600,
                 source => "puppet:///modules/ssh/sshd_config";
 
 		'/etc/ssh/ssh_config':
                 ensure => present,
                 owner => 'root',
-                group => root,
+                group => 'root',
                 mode => 644,
                 source => "puppet:///modules/ssh/ssh_config";
 		
 		['/usr/sbin/sshd','/usr/libexec/openssh/ssh-keysign','/usr/libexec/openssh/sftp-server','/usr/bin/ssh-keyscan','/usr/bin/ssh-agent','/usr/bin/ssh-keygen','/usr/bin/ssh-add','/usr/bin/ssh','/usr/bin/sftp','/usr/bin/scp',]:
-                owner   => root,
-                group   => root,
+                owner   => 'root',
+                group   => 'root',
                 mode    => "g-w,o-w",
 	}
 }
